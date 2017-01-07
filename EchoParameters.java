086f91fad653e7856a35066a0de3fe49d6e4ca87
tsira <tsira@users.noreply.github.com>
2017-01-06 16:06:26 -1000

    Add files via upload
    baseVersion

---
+++ EchoParameters.java
@@ -0,0 +1,23 @@
+
+
+public class EchoParameters {
+
+	public static void main (String [] args){
+	
+		
+		for (int x=0; x< args.length; x++){
+			
+			System.out.println(args[x]);
+			
+			
+		};
+		
+		
+			                  
+		
+	
+	}
+	
+	
+	
+}
